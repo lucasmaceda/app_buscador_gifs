0ea7c58b883980ae0bc438e92135d6b455fcbf91
Lucas Maceda Miranda <lucasmacedamiranda@gmail.com>
2021-06-08 20:54:53 -0300

    first commit

---
+++ android/app/src/main/java/com/example/app_buscador_gifs/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.app_buscador_gifs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'ui/homepage.dart';
+
+void main() {
+  runApp(
+      MaterialApp(home: HomePage(), theme: ThemeData(hintColor: Colors.white)));
+}
